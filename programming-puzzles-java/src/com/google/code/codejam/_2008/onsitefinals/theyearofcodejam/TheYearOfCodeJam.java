97a6277d11408bf1ebcf4e7ffd06b00e4d823608
nabeelmukhtar <nabeelmukhtar@c5345448-26a3-11df-9191-49dca8b91aeb>
2010-03-03 12:14:13 +0000

    Initial commit of programming-puzzles-java

---
+++ programming-puzzles-java/src/com/google/code/codejam/_2008/onsitefinals/theyearofcodejam/TheYearOfCodeJam.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.google.code.codejam._2008.onsitefinals.theyearofcodejam;
+
+import com.google.code.codejam.common.BaseCommandLineClient;
+
+/**
+ * @author nmukhtar
+ *
+ */
+public class TheYearOfCodeJam extends BaseCommandLineClient {
+
+	/**
+	 * @param lineNumber
+	 * @param lineTokens
+	 */
+	@Override
+	public void processTokens(int lineNumber, String[] lineTokens) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+    	try {
+            new TheYearOfCodeJam().run(args);
+            System.exit(0);
+    	} catch (Throwable t) {
+    		t.printStackTrace();
+    		System.exit(1);
+    	}
+	}
+}
