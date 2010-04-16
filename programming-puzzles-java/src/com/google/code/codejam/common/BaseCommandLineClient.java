a7d8cb2bbe1f62852f8e39c2d0ac3eb7be0f2bab
nabeelmukhtar <nabeelmukhtar@c5345448-26a3-11df-9191-49dca8b91aeb>
2010-04-16 15:22:15 +0000

    Alien language.

--- programming-puzzles-java/src/com/google/code/codejam/common/BaseCommandLineClient.java
+++ programming-puzzles-java/src/com/google/code/codejam/common/BaseCommandLineClient.java
@@ -5,15 +5,19 @@ package com.google.code.codejam.common;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.Reader;
+import java.io.Writer;
 
 /**
  * @author nmukhtar
- *
+ * 
  */
 public abstract class BaseCommandLineClient {
 	protected String delimiter = "\\s";
+	protected PrintWriter writer;
 
 	/**
 	 * @param args
@@ -25,43 +29,67 @@ public abstract class BaseCommandLineClient {
 			BufferedReader reader = null;
 			try {
 				reader = new BufferedReader(new FileReader(args[0]));
-		    	String line = null;
-		    	int lineNumber = 0;
-	        	while((line = reader.readLine()) != null) {
-	        		String[] tokens = line.split(delimiter);
-	        		processTokens(++lineNumber, tokens);
-	        	}
+				writer = new PrintWriter(new FileWriter(args[1]));
+				String line = null;
+				int lineNumber = 0;
+				while ((line = reader.readLine()) != null) {
+					String[] tokens = line.split(delimiter);
+					processTokens(++lineNumber, tokens);
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			} finally {
 				closeReader(reader);
+				closeWriter(writer);
 			}
 		}
 	}
-	
+
 	/**
-	 *
+	 * 
 	 */
 	protected void printHelp() {
-		System.err.println("The syntax to run this command line application is:");
-		System.err.println("java " + getClass().getName() + " <inputfilename>");
+		System.err
+				.println("The syntax to run this command line application is:");
+		System.err.println("java " + getClass().getName()
+				+ " <inputfilename> <outputfilename>");
 	}
-	
+
 	/**
-	 *
+	 * 
 	 */
-    protected void closeReader(Reader reader) {
-    	if (reader != null) {
-            try {
-            	reader.close();
-            } catch (IOException e) {
-            	e.printStackTrace();	
-            }
-    	}
-    }
-	
+	protected void printLine(String line) {
+		writer.println(line);
+	}
+
 	/**
-	 *
+	 * 
+	 */
+	protected void closeReader(Reader reader) {
+		if (reader != null) {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * 
+	 */
+	protected void closeWriter(Writer writer) {
+		if (writer != null) {
+			try {
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * 
 	 */
 	public abstract void processTokens(int lineNumber, String[] lineTokens);
 }
