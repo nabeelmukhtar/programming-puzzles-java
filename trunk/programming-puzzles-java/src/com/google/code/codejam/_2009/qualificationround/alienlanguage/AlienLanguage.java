cacd66d0fc1472c6b701f54c5cd16a1b1a4a04f2
nabeelmukhtar <nabeelmukhtar@c5345448-26a3-11df-9191-49dca8b91aeb>
2010-04-16 15:22:15 +0000

    Alien language.

--- trunk/programming-puzzles-java/src/com/google/code/codejam/_2009/qualificationround/alienlanguage/AlienLanguage.java
+++ trunk/programming-puzzles-java/src/com/google/code/codejam/_2009/qualificationround/alienlanguage/AlienLanguage.java
@@ -3,6 +3,10 @@
  */
 package com.google.code.codejam._2009.qualificationround.alienlanguage;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 import com.google.code.codejam.common.BaseCommandLineClient;
 
 /**
@@ -10,6 +14,10 @@ import com.google.code.codejam.common.BaseCommandLineClient;
  *
  */
 public class AlienLanguage extends BaseCommandLineClient {
+	private int numberOfTokens = 0;
+	private int numberOfWords = 0;
+	private int numberOfSamples = 0;
+	private List<String> dictionary = new ArrayList<String>();
 
 	/**
 	 * @param lineNumber
@@ -17,7 +25,35 @@ public class AlienLanguage extends BaseCommandLineClient {
 	 */
 	@Override
 	public void processTokens(int lineNumber, String[] lineTokens) {
-		// TODO Auto-generated method stub
+		if (lineNumber == 1) {
+			numberOfTokens = Integer.parseInt(lineTokens[0]);
+			numberOfWords = Integer.parseInt(lineTokens[1]);
+			numberOfSamples = Integer.parseInt(lineTokens[2]);
+		} else if (dictionary.size() < numberOfWords) {
+			dictionary.add(lineTokens[0]);
+		} else {
+			int matchCount = 0;
+			for (String word : dictionary) {
+				if (Pattern.matches(convertToRegex(lineTokens[0]), word)) {
+					matchCount ++;
+				}
+			}
+			printLine(String.format("Case #%d: %d\n", lineNumber - numberOfWords - 1, matchCount));
+		}
+	}
+
+	private String convertToRegex(String string) {
+		StringBuilder builder = new StringBuilder();
+		for (char c : string.toCharArray()) {
+			if (c == '(') {
+				builder.append('[');
+			} else if (c == ')') {
+				builder.append(']');
+			} else {
+				builder.append(c);
+			}
+		}
+		return builder.toString();
 	}
 
 	/**
