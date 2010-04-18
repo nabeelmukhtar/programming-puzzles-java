caa207b5f27e6a0078a937a0e333350144ac3fa3
nabeelmukhtar <nabeelmukhtar@c5345448-26a3-11df-9191-49dca8b91aeb>
2010-04-18 15:22:38 +0000

    changed base class

--- programming-puzzles-java/src/com/google/code/codejam/_2009/onlineround3/alphabetomials/Alphabetomials.java
+++ programming-puzzles-java/src/com/google/code/codejam/_2009/onlineround3/alphabetomials/Alphabetomials.java
@@ -3,6 +3,9 @@
  */
 package com.google.code.codejam._2009.onlineround3.alphabetomials;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
 import com.google.code.codejam.common.BaseCommandLineClient;
 
 /**
@@ -12,11 +15,9 @@ import com.google.code.codejam.common.BaseCommandLineClient;
 public class Alphabetomials extends BaseCommandLineClient {
 
 	/**
-	 * @param lineNumber
-	 * @param lineTokens
 	 */
 	@Override
-	public void processTokens(int lineNumber, String[] lineTokens) {
+	public void process(BufferedReader input, PrintWriter output) throws Exception {
 		// TODO Auto-generated method stub
 	}
 
