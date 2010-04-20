5963551a3f0c24f74b0a55b1b9f268b6612ad8d1
nabeelmukhtar <nabeelmukhtar@c5345448-26a3-11df-9191-49dca8b91aeb>
2010-04-20 08:51:05 +0000

    changing the directory of problems

---
+++ trunk/programming-puzzles-java/src/com/google/code/codejam/_2008/qualificationround/FlySwatter.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.google.code.codejam._2008.qualificationround;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+import com.google.code.codejam.common.BaseCommandLineClient;
+
+/**
+ * @author nmukhtar
+ *
+ */
+public class FlySwatter extends BaseCommandLineClient {
+
+	/**
+	 */
+	@Override
+	public void process(BufferedReader input, PrintWriter output) throws Exception {
+		final int numberOfTestCases = Integer.parseInt(input.readLine());
+		for (int i = 0; i < numberOfTestCases; i++) {
+			output.println(String.format("Case #%d: %g", (i + 1), getProbability(input)));
+		}
+	}
+
+	private double getProbability(BufferedReader input) throws Exception {
+		double probability = 0.0;
+		String[] tokens = input.readLine().split(delimiter);
+		double f = Double.parseDouble(tokens[0]);
+		double R = Double.parseDouble(tokens[1]);
+		double t = Double.parseDouble(tokens[2]);
+		double r = Double.parseDouble(tokens[3]);
+		double g = Double.parseDouble(tokens[4]);
+		
+		// TODO Auto-generated method stub
+		return probability;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+    	try {
+            new FlySwatter().run(args);
+            System.exit(0);
+    	} catch (Throwable t) {
+    		t.printStackTrace();
+    		System.exit(1);
+    	}
+	}
+}
