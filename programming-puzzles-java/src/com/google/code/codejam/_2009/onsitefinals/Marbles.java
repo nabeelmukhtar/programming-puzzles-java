e8376a0275790e90efe5feb9c2228b26a44685b4
nabeelmukhtar <nabeelmukhtar@c5345448-26a3-11df-9191-49dca8b91aeb>
2010-04-20 09:44:06 +0000

    changing the directory of problems

---
+++ programming-puzzles-java/src/com/google/code/codejam/_2009/onsitefinals/Marbles.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.google.code.codejam._2009.onsitefinals;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+import com.google.code.codejam.common.BaseCommandLineClient;
+
+/**
+ * @author nmukhtar
+ *
+ */
+public class Marbles extends BaseCommandLineClient {
+
+	/**
+	 */
+	@Override
+	public void process(BufferedReader input, PrintWriter output) throws Exception {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+    	try {
+            new Marbles().run(args);
+            System.exit(0);
+    	} catch (Throwable t) {
+    		t.printStackTrace();
+    		System.exit(1);
+    	}
+	}
+}
