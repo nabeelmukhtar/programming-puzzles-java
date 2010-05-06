ccf81eafa883ff479a7950dbf14117b61039c79c
nabeelmukhtar <nabeelmukhtar@c5345448-26a3-11df-9191-49dca8b91aeb>
2010-05-06 12:18:12 +0000

    Not working solutions...

--- trunk/programming-puzzles-java/src/com/google/code/codejam/_2009/onlineround1_subroundB/SquareMath.java
+++ trunk/programming-puzzles-java/src/com/google/code/codejam/_2009/onlineround1_subroundB/SquareMath.java
@@ -4,6 +4,7 @@
 package com.google.code.codejam._2009.onlineround1_subroundB;
 
 import java.io.PrintWriter;
+import java.util.Comparator;
 import java.util.Scanner;
 
 import com.google.code.codejam.common.BaseCommandLineClient;
@@ -18,7 +19,41 @@ public class SquareMath extends BaseCommandLineClient {
 	 */
 	@Override
 	public void process(Scanner input, PrintWriter output) throws Exception {
-		// TODO Auto-generated method stub
+		int T = Integer.parseInt(input.nextLine());
+		for (int i = 1; i <= T; i++) {
+			output.printf("Case #%d:\n", i);
+			printPaths(input, output);
+		}
+	}
+
+	private void printPaths(Scanner input, PrintWriter output) {
+		String[] tokens = input.nextLine().split(delimiter);
+		int W = Integer.parseInt(tokens[0]);
+		int Q = Integer.parseInt(tokens[1]);
+		char[][] arithmeticSquare = new char[W][W];
+		for (int i = 0; i < W; i++) {
+			arithmeticSquare[i] = input.nextLine().toCharArray();
+		}
+		tokens = input.nextLine().split(delimiter);
+		for (int i = 0; i < tokens.length; i++) {
+			printPath(arithmeticSquare, Integer.parseInt(tokens[i]), output);
+		}
+	}
+
+	private void printPath(char[][] arithmeticSquare, int query, PrintWriter output) {
+	}
+	
+	private int applyOperator(char op, int i, int j) {
+		switch (op) {
+		case '+':
+			return i + j;
+
+		case '-':
+			return i - j;
+			
+		default:
+			throw new IllegalArgumentException("Illegal operator found:" + op);
+		}		
 	}
 
 	/**
@@ -32,5 +67,17 @@ public class SquareMath extends BaseCommandLineClient {
     		t.printStackTrace();
     		System.exit(1);
     	}
+	}
+	
+	public static class PathComparator implements Comparator<String> {
+
+		@Override
+		public int compare(String o1, String o2) {
+			if (o1.length() != o2.length()) {
+				return o1.length() - o2.length();
+			} else {
+				return o1.compareTo(o2);
+			}
+		}
 	}
 }
--- trunk/programming-puzzles-java/src/com/google/code/codejam/_2009/onlineround1_subroundB/TheNextNumber.java
+++ trunk/programming-puzzles-java/src/com/google/code/codejam/_2009/onlineround1_subroundB/TheNextNumber.java
@@ -4,6 +4,7 @@
 package com.google.code.codejam._2009.onlineround1_subroundB;
 
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import com.google.code.codejam.common.BaseCommandLineClient;
@@ -18,7 +19,55 @@ public class TheNextNumber extends BaseCommandLineClient {
 	 */
 	@Override
 	public void process(Scanner input, PrintWriter output) throws Exception {
-		// TODO Auto-generated method stub
+		int T = Integer.parseInt(input.nextLine());
+		for (int i = 1; i <= T; i++) {
+			output.printf("Case #%d: %s\n", i, getNextNumber(input));			
+		}
+	}
+
+	private String getNextNumber(Scanner input) {
+		String integer = input.nextLine();
+		char[] digits = integer.toCharArray();
+		for (int i = digits.length - 2; i > 0; i--) {
+			int selected = -1;
+			for (int j = i + 1; j < digits.length; j++) {
+				if (selected < 0 || (digits[j] < digits[selected] && digits[j] > digits[i])) {
+					selected = j;
+				}
+			}
+			if (selected >= 0 && digits[selected] > digits[i]) {
+				char temp = digits[selected];
+				digits[selected] = digits[i];
+				digits[i] = temp;
+				char[] tempArr = Arrays.copyOfRange(digits, i + 1, digits.length);
+				Arrays.sort(tempArr);
+				for (int j = i + 1; j < digits.length; j++) {
+					digits[j] = tempArr[j - i - 1];
+				}
+				return String.valueOf(digits);
+			}
+		}
+		Arrays.sort(digits);
+		char[] tempArr = new char[digits.length + 1];
+		int nonzeroIndex = 0;
+
+		for (int i = 0; i < digits.length; i++) {
+			if (i == 0) {
+				tempArr[i] = digits[i];
+			} else {
+				tempArr[i + 1] = digits[i];
+			}
+			if (digits[i] != '0' && nonzeroIndex == 0) {
+				nonzeroIndex = i + 1;
+			}
+		}
+		
+		tempArr[1] = '0';
+		if (nonzeroIndex != 0) {
+			tempArr[0] = tempArr[nonzeroIndex];
+			tempArr[nonzeroIndex] = '0';
+		}
+		return String.valueOf(tempArr);
 	}
 
 	/**
