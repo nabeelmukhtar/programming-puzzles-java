d095f2825ffd155bb5fad804408c10c2112e1f4f
nabeelmukhtar <nabeelmukhtar@c5345448-26a3-11df-9191-49dca8b91aeb>
2010-05-06 12:18:12 +0000

    Not working solutions...

--- programming-puzzles-java/src/com/google/code/codejam/_2009/onlineround1_subroundA/MultibaseHappiness.java
+++ programming-puzzles-java/src/com/google/code/codejam/_2009/onlineround1_subroundA/MultibaseHappiness.java
@@ -18,7 +18,47 @@ public class MultibaseHappiness extends BaseCommandLineClient {
 	 */
 	@Override
 	public void process(Scanner input, PrintWriter output) throws Exception {
-		// TODO Auto-generated method stub
+		int T = Integer.parseInt(input.nextLine());
+		for (int i = 0; i < T; i++) {
+			output.printf("Case #%d: %d\n", (i + 1), getHappyNumber(input.nextLine()));			
+		}
+	}
+
+	private int getHappyNumber(String nextLine) {
+		String[] tokens = nextLine.split(delimiter);
+		int[] bases = new int[tokens.length];
+		for (int i = 0; i < bases.length; i++) {
+			bases[i] = Integer.parseInt(tokens[i]);
+		}
+		int happyNumber = 2;
+		while (true) {
+			boolean found = true;
+			for (int i = 0; i < bases.length; i++) {
+				int sum = calculateSum(Integer.toString(happyNumber, bases[i]).toCharArray(), bases[i]);
+				if (sum != 1) {
+					found = false;
+					happyNumber++;
+					break;
+				}
+			}
+			if (found) {
+				break;
+			}
+		}
+		return happyNumber;
+	}
+
+	private int calculateSum(char[] charArray, int i) {
+		int sum = 0;
+		for (int j = 0; j < charArray.length; j++) {
+			sum += (int) charArray[j] - 48;
+		}
+		char[] charArray2 = Integer.toString(sum, i).toCharArray();
+		if (charArray2.length > 1) {
+			sum = calculateSum(charArray2, i);
+		}
+		
+		return sum;
 	}
 
 	/**
