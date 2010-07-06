24b4224581c5de1336c3e1b7d04bc13e6572e9bb
nabeelmukhtar <nabeelmukhtar@c5345448-26a3-11df-9191-49dca8b91aeb>
2010-07-06 06:33:48 +0000

    CodeJam 2010 Puzzles.

---
+++ trunk/programming-puzzles-java/src/com/google/code/codejam/_2010/round1_A/Rotate.java
@@ -0,0 +1,38 @@
+package com.google.code.codejam._2010.round1_A;
+/**
+ * 
+ */
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import com.google.code.codejam.common.BaseCommandLineClient;
+
+/**
+ * @author nmukhtar
+ * 
+ */
+public class Rotate extends BaseCommandLineClient {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+    	try {
+            new Rotate().run(args);
+            System.exit(0);
+    	} catch (Throwable t) {
+    		t.printStackTrace();
+    		System.exit(1);
+    	}
+	}
+
+	/**
+	 * @param input TODO
+	 * @param output TODO
+	 * @throws Exception TODO
+	 * 
+	 */
+	public void process(Scanner input, PrintWriter output) throws Exception {
+		// TODO-NM: Solve this puzzle.
+	}
+}
---
+++ trunk/programming-puzzles-java/src/com/google/code/codejam/_2010/round2/GrazingGoogleGoats.java
@@ -0,0 +1,38 @@
+package com.google.code.codejam._2010.round2;
+/**
+ * 
+ */
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import com.google.code.codejam.common.BaseCommandLineClient;
+
+/**
+ * @author nmukhtar
+ * 
+ */
+public class GrazingGoogleGoats extends BaseCommandLineClient {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+    	try {
+            new GrazingGoogleGoats().run(args);
+            System.exit(0);
+    	} catch (Throwable t) {
+    		t.printStackTrace();
+    		System.exit(1);
+    	}
+	}
+
+	/**
+	 * @param input TODO
+	 * @param output TODO
+	 * @throws Exception TODO
+	 * 
+	 */
+	public void process(Scanner input, PrintWriter output) throws Exception {
+		// TODO-NM: Solve this puzzle.
+	}
+}
