378df78922f2c5e6af73d7b0f9f7d78a71b7c596
nabeelmukhtar <nabeelmukhtar@c5345448-26a3-11df-9191-49dca8b91aeb>
2011-05-24 07:10:46 +0000

    CodeJam 2011 Files.

---
+++ trunk/programming-puzzles-java/src/com/google/code/codejam/_2011/round1_B/RevengeOfTheHotDogs.java
@@ -0,0 +1,38 @@
+package com.google.code.codejam._2011.round1_B;
+/**
+ * 
+ */
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import com.google.code.codejam.common.BaseCommandLineClient;
+
+/**
+ * @author nmukhtar
+ * 
+ */
+public class RevengeOfTheHotDogs extends BaseCommandLineClient {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+    	try {
+            new RevengeOfTheHotDogs().run(args);
+            System.exit(0);
+    	} catch (Throwable t) {
+    		t.printStackTrace();
+    		System.exit(1);
+    	}
+	}
+
+	/**
+	 * @param input TODO
+	 * @param output TODO
+	 * @throws Exception TODO
+	 * 
+	 */
+	public void process(Scanner input, PrintWriter output) throws Exception {
+		// TODO-NM: Solve this puzzle.
+	}
+}
---
+++ trunk/programming-puzzles-java/src/com/google/code/codejam/_2011/round1_C/PerfectHarmony.java
@@ -0,0 +1,38 @@
+package com.google.code.codejam._2011.round1_C;
+/**
+ * 
+ */
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import com.google.code.codejam.common.BaseCommandLineClient;
+
+/**
+ * @author nmukhtar
+ * 
+ */
+public class PerfectHarmony extends BaseCommandLineClient {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+    	try {
+            new PerfectHarmony().run(args);
+            System.exit(0);
+    	} catch (Throwable t) {
+    		t.printStackTrace();
+    		System.exit(1);
+    	}
+	}
+
+	/**
+	 * @param input TODO
+	 * @param output TODO
+	 * @throws Exception TODO
+	 * 
+	 */
+	public void process(Scanner input, PrintWriter output) throws Exception {
+		// TODO-NM: Solve this puzzle.
+	}
+}
