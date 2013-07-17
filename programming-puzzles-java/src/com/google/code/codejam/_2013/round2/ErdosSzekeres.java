7d25956a3d025332ca137e36791ebd2266f1e24c
nabeelmukhtar <nabeelmukhtar@c5345448-26a3-11df-9191-49dca8b91aeb>
2013-07-17 15:09:58 +0000

    Stubs for code jam 2012 and 201.

---
+++ programming-puzzles-java/src/com/google/code/codejam/_2013/round2/ErdosSzekeres.java
@@ -0,0 +1,38 @@
+package com.google.code.codejam._2013.round2;
+/**
+ * 
+ */
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import com.google.code.codejam.common.BaseCommandLineClient;
+
+/**
+ * @author nmukhtar
+ * 
+ */
+public class ErdosSzekeres extends BaseCommandLineClient {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+    	try {
+            new ErdosSzekeres().run(args);
+            System.exit(0);
+    	} catch (Throwable t) {
+    		t.printStackTrace();
+    		System.exit(1);
+    	}
+	}
+
+	/**
+	 * @param input TODO
+	 * @param output TODO
+	 * @throws Exception TODO
+	 * 
+	 */
+	public void process(Scanner input, PrintWriter output) throws Exception {
+		// TODO-NM: Solve this puzzle.
+	}
+}
---
+++ programming-puzzles-java/src/com/google/code/codejam/_2013/worldfinals/Runs.java
@@ -0,0 +1,38 @@
+package com.google.code.codejam._2013.worldfinals;
+/**
+ * 
+ */
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import com.google.code.codejam.common.BaseCommandLineClient;
+
+/**
+ * @author nmukhtar
+ * 
+ */
+public class Runs extends BaseCommandLineClient {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+    	try {
+            new Runs().run(args);
+            System.exit(0);
+    	} catch (Throwable t) {
+    		t.printStackTrace();
+    		System.exit(1);
+    	}
+	}
+
+	/**
+	 * @param input TODO
+	 * @param output TODO
+	 * @throws Exception TODO
+	 * 
+	 */
+	public void process(Scanner input, PrintWriter output) throws Exception {
+		// TODO-NM: Solve this puzzle.
+	}
+}
